f105f996343b54d8eb03cefea8ab016147faabf5
chadwilcomb <chadwilcomb@gmail.com>
2014-12-03 15:08:43 -0500

    using default for wellknown module
    pointing to root for wellknown module (use index.js) instead of using
    wellknown.js

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 "use strict";
 var _ = require('underscore');
-var WKT = require('wellknown/wellknown.js');
+var WKT = require('wellknown');
 var proj4 = require('proj4');
 var nysp = "+proj=lcc +lat_1=41.03333333333333 +lat_2=40.66666666666666 +lat_0=40.16666666666666 +lon_0=-74 +x_0=300000.0000000001 +y_0=0 +ellps=GRS80 +datum=NAD83 +to_meter=0.3048006096012192 +no_defs";
 var webMerc = "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs";
-//var $ = require('jquery');
-
-
 
 var convertNYSP2WebMerc = function (geojson) {
     geojson = geojson.geometry || geojson;
